9ee70acf5189176c8620a2d61426c50c57fe2ed3
Dongwoo Kim <kimdwkimdw@gmail.com>
2014-04-28 17:00:46 +0900

    multiple log 기능

--- RemoteLogger.js
+++ RemoteLogger.js
@@ -3,9 +3,20 @@ var url = require('url')
 
 var winston = require('winston');
 
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.File)({ filename: 'url.log' })
+  ]
+});
+
 winston.loggers.add('url_logger', {
+  console: {
+    level: 'silly',
+    colorize: 'true',
+    label: 'url'
+  },
   file: {
-    filename: 'url.log'
+    filename: 'log/url.log'
   }
 });
 
@@ -14,10 +25,10 @@ var url_logger = winston.loggers.get('url_logger');
 http.createServer(function(req, res) {
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-
+  
   if (query.url) {
-    url_logger.log(query.url)
-    winston.info(query.url)
+    logger.info("", {url:query.url})
+    url_logger.info("", {url:query.url})
   }
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.writeHead(200, {'Content-Type': 'application/javascript'});
