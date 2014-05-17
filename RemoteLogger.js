3f4aa319576682d9a2935a8ee216b3c53b9df015
Dongwoo Arthur Kim <kimdwkimdw@gmail.com>
2014-05-17 19:18:14 +0900

    remoteAddress

--- RemoteLogger.js
+++ RemoteLogger.js
@@ -25,12 +25,11 @@ var url_logger = winston.loggers.get('url_logger');
 http.createServer(function(req, res) {
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-  
   if (query.url) {
-    logger.info("", {url:query.url})
+    logger.info("", {url:query.url, from: req.connection.remoteAddress})
     url_logger.info("", {url:query.url})
   }
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.writeHead(200, {'Content-Type': 'application/javascript'});
   res.end(query.callback+'({"url":"'+query.url+'"})');
-}).listen(8080);
+}).listen(18000);
